c2cbb553a7015219d0c3f7906bfb749f2ad17ded
rocktyy <tianyuyan@163.com>
2022-04-24 03:26:40 +0800

    first commit

---
+++ nftTest.js
@@ -0,0 +1,15 @@
+import * as Web3 from 'web3'
+import { OpenSeaPort, Network } from 'opensea-js'
+
+// This example provider won't let you make transactions, only read-only calls:
+const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io')
+
+const seaport = new OpenSeaPort(provider, {
+  networkName: Network.Main,
+  apiKey: YOUR_API_KEY
+})
+
+console.log("++++++++++++++++seaport+++++++++");
+console.log(seaport);
+
+
